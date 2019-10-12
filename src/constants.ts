511790575237123ae4197205772f8b146bf44e52
Grant Blakeman <hello@grantblakeman.com>
2019-10-11 18:52:46 -0700

    stub in basic plugin architecture

---
+++ src/constants.ts
@@ -0,0 +1,90 @@
+/* eslint-disable import/prefer-default-export */
+
+/**
+ * @description A unique string to identify the plugin within Figma.
+ * Changing this will break data retrieval in any `sharedPluginData` and potentially elsewhere.
+ * [More info]{@link https://www.figma.com/plugin-docs/api/properties/nodes-setsharedplugindata/}
+ *
+ * @kind constant
+ * @name PLUGIN_IDENTIFIER
+ * @type {string}
+ */
+const PLUGIN_IDENTIFIER = 'com.linkedin.figma.specter-plugin';
+
+/**
+ * @description The public-facing name for the plugin. This should match the
+ * `name` stated in manifset.json.
+ *
+ * @kind constant
+ * @name PLUGIN_NAME
+ * @type {string}
+ */
+const PLUGIN_NAME = 'Exonym';
+
+/**
+ * @description An object containing the sets of typefaces in-use by the plugin.
+ *
+ * @kind constant
+ * @name TYPEFACES
+ * @type {Object}
+ */
+const TYPEFACES = {
+  primary: {
+    family: 'Helvetica Neue',
+    style: 'Regular',
+  },
+  secondary: {
+    family: 'Roboto',
+    style: 'Regular',
+  },
+};
+
+/**
+ * @description An object containing the current string constants the Figma API returns for
+ * top-level (`main`) layer and `group` layer types.
+ *
+ * @kind constant
+ * @name FRAME_TYPES
+ * @type {Object}
+ */
+const FRAME_TYPES = {
+  main: 'FRAME',
+  group: 'GROUP',
+};
+
+/**
+ * @description An object containing `height`/`width` settings for the plugin GUI window.
+ *
+ * @kind constant
+ * @name GUI_SETTINGS
+ * @type {Object}
+ */
+const GUI_SETTINGS = {
+  default: {
+    width: 140,
+    height: 180,
+  },
+  input: {
+    width: 440,
+    height: 145,
+  },
+};
+
+/**
+ * @description A unique message that will cause the plugin to shut-down.
+ *
+ * @kind constant
+ * @name CLOSE_PLUGIN_MSG
+ * @type {string}
+ */
+const CLOSE_PLUGIN_MSG = '_CLOSE_PLUGIN_';
+
+export {
+  CLOSE_PLUGIN_MSG,
+  FRAME_TYPES,
+  GUI_SETTINGS,
+  PLUGIN_IDENTIFIER,
+  PLUGIN_NAME,
+  TYPEFACES,
+};
+/* eslint-enable import/prefer-default-export */
