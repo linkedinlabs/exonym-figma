d4ac2196249b05942f1302f243cfb4384dae1155
Grant Blakeman <hello@grantblakeman.com>
2020-01-22 11:41:16 -0800

    js - vendor - update figma-style select base

--- src/GUI.ts
+++ src/GUI.ts
@@ -96,9 +96,6 @@ const initLanguages = (): void => {
 
     // add additional options to group
     addlLanguages.forEach(language => addOption(language, addlOptGroupElement));
-
-    // set up the Figma version
-    selectMenu.init({ position: 'overlap' });
   }
 
   return null;
@@ -201,7 +198,7 @@ const setOptions = (options: {
   // remove the Figma version so it can be reset
   // the figma-select-menu make a <select> clone, so it must be removed before selecting
   // the menu from the DOM
-  selectMenu.destroy();
+  selectMenu.destroy({ selector: 'select-menu' });
 
   const languageIndex = 0; // currently GUI only supports 1 language at a time; take first
   const language = languages[languageIndex];
@@ -218,7 +215,7 @@ const setOptions = (options: {
     }
 
     // set the Figma version of the menu
-    selectMenu.init({ position: 'overlap' });
+    selectMenu.init({ selector: 'select-menu', position: 'overlap' });
   }
 
   if (textActionElement) {
